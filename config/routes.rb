0817482d4fdb31182457e47e798a4535894f312a
Thomas <thomasdanielrice@proton.me>
2026-05-25 14:41:24 +0200

    Added routes, views, controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: %i[new create destroy]
+  resources :students, only: %i[new create destroy show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
